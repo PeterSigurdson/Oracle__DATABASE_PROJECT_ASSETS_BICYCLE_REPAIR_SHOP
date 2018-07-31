c98a993b2ce21ecfb5d3618fbff983b2f8006cd1
Peter Sigurdson <peter@petersigurdson.net>
2018-07-31 14:23:16 -0400

    20180731B

--- left_join_1.sql
+++ left_join_1.sql
@@ -1,6 +1,12 @@
-select bc_customers.cellphone, bc_customers.city, bc_customers.occupation
-bc_purchase_records.purchase_price, bc_purchase_records.bike_id
+create table  BC_OUTPUT_A
+AS ( 
+select bc_customers.cellphone, bc_customers.city, bc_customers.occupation, 
+bc_purchase_records.purchase_price, bc_purchase_records.bike_id, 
+bc_repair_data.date_in, bc_repair_data.charge, bc_repair_data.bike_id 
 
 from bc_customers
 
-left join bc_purchase_records on bc_customers.cellphone = bc_purchase_records.cellphone;
+left join bc_purchase_records on bc_customers.cellphone = bc_purchase_records.cellphone
+
+left join bc_repair_data on bc_customers.cellphone = bc_repair_data.cellphone)
+;
